a74a3a8abf7f31f43d0bc302a1ecada30eb91977
DS <saumil.doshi7@gmail.com>
2024-11-21 13:28:56 +0530

    Add files via upload

---
+++ Task_Saumil_Doshi/Resources/AppConstant.swift
@@ -0,0 +1,20 @@
+//
+//  AppConstant.swift
+//  Demo App Task - Elluminati
+//
+//  Created by Demo App on 23/10/24.
+//
+
+import UIKit
+
+
+struct API_NAME {
+    static let channelSliders = "channel-sliders"
+    static let channelList = "channel-list?typeurl=mpd"
+}
+
+struct BASE_URL {
+    static let apartmentSize = "https://new.staging.streamlytv.com/api/auth/"
+}
+    
+    
